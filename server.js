1b50fc466d0afa2becf684b8c10589dca257e7ff
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-04 13:56:58 +0900

    feat: Add a listen event function

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ server.on('connection', (socket) => {
     console.log(`Client local address : ${socket.localAddress}, local port : ${socket.localPort}`);
     console.log(`Client remote address : ${socket.remoteAddress}, remote port : ${socket.remotePort}`);
 });
+
+server.listen(50000, () => {
+    console.log(`TCP Server listen on address ${JSON.stringify(server.address())}`);
+});
