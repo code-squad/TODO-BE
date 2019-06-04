661f3fc076d4a9374fef9e0a9df2296bd8d37eee
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-04 13:54:38 +0900

    feat: Add a connection event function

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const net = require('net');
 const server = net.createServer();
+
+server.on('connection', (socket) => {
+    console.log(`Client Connect!`);
+    console.log(`Client local address : ${socket.localAddress}, local port : ${socket.localPort}`);
+    console.log(`Client remote address : ${socket.remoteAddress}, remote port : ${socket.remotePort}`);
+});
