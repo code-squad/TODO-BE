69ae9da3665df9fb3ddaada9dcd7bef4fbfdeb3f
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-04 14:13:55 +0900

    feat: Add a data event function (client.js)

--- client.js
+++ client.js
@@ -3,3 +3,7 @@ const client = net.createConnection({ host: 'localhost', port: 50000 }, () => {
     console.log(`local address : ${client.localAddress}, local port : ${client.localPort}`);
     console.log(`remote address : ${client.remoteAddress}, remote port : ${client.remotePort}`);
 });
+
+client.on('data', (data) => {
+    console.log(`server send data : ${data}`);
+});
