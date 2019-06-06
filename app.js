ee2f7ac76afe3d332c37794ed987802f88219848
Sony <sonypark0204@gmail.com>
2019-06-06 16:05:56 +0900

    [feat] Create TodoApp class
    - todo mainExecutor 생성

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ const inputReadline = rl.createInterface({
 });
 
 class TodoApp{
+    mainExecutor() {
+        inputReadline.setPrompt('명령어를 입력하세요(도움말은 help / 종료하려면 q를 누르세요): ');
+        inputReadline.prompt();
+        inputReadline.on('line', function (line) {
 
+            if (line === "q") inputReadline.close();
+            todoList.checkCommands(line);
+
+            if (line === 'no') {
+                todoList.register()
+            }
+        })
+
+            .on('close', function () {
+                console.log('프로그램이 종료되었습니다.');
+                process.exit();
+            });
+    }
 }
+
+const todoList = new TodoApp();
+todoList.mainExecutor();
