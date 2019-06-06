4cd251a4628ca58ab21a7bcb2aa9f4b4e2c53f4a
Sony <sonypark0204@gmail.com>
2019-06-06 16:21:48 +0900

    [feat] Create getTodos method
    - todo list 출력 함수 생성

--- app.js
+++ app.js
@@ -97,6 +97,19 @@ class TodoApp{
                 .write();
         });
     }
+
+    getTodos() {
+        const todos = db.get('todos').value();
+        let index = 1;
+        todos.forEach(todo => {
+            let todoText = `${index++}. ${todo.title}`
+            if (todo.complete) {
+                todoText += ' ✔ ️'
+            }
+            console.log(chalk.strikethrough(todoText))
+        });
+    }
+
 }
 
 const todoList = new TodoApp();
