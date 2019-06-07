336730ec76ee1083d570686e8ac6b2bb38363e44
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-07 17:30:16 +0900

    feat: Add a 'sign in' function

--- member_client.js
+++ member_client.js
@@ -1,2 +1,18 @@
 const net = require('net');
 const memberClient = net.createConnection({ host: 'localhost', port:60000 });
+
+const send = (data) => { memberClient.write(data); }
+
+const receive = () => {
+    return new Promise((resolve) => { 
+        memberClient.on('data', (data) => { resolve(Boolean(JSON.stringify(data))); }); 
+    });
+}
+
+const signIn = async (userID, userPW) => {
+    send(userID + '#' + userPW);
+    if (!await receive()) return false;
+    return true;
+}
+
+module.exports = { signIn };
