79326b2e065a408ef2ae8c5dfcedb7ba9f44404c
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-07 23:19:16 +0900

    test: Simulate a  multi echo member server

--- member_server.js
+++ member_server.js
@@ -1,17 +1,31 @@
+const fileManager = require('./file_manager');
 const net = require('net');
+
 const memberServer = net.createServer();
 const memberClientSocket = [];
 
+memberServer.listen(60000, () => { console.log(`[Member] Server listen on address => ${JSON.stringify(memberServer.address())}`); });
+
 memberServer.on('connection', (memberClient) => {
-    console.log(`Member Client Connect!`);
-    console.log(`Member Client local address : ${memberClient.localAddress}, local port : ${memberClient.localPort}`);
-    console.log(`Member Client remote address : ${memberClient.remoteAddress}, remote port : ${memberClient.remotePort}`);
+    console.log(`[Member] Client local address => ${memberClient.localAddress}, local port => ${memberClient.localPort}`);
+    console.log(`[Member] Client remote address => ${memberClient.remoteAddress}, remote port => ${memberClient.remotePort}`);
 
     memberClientSocket.push(memberClient);
 
     memberClient.on('data', (data) => {
-        console.log(`Member Server receive data : ${data}`);
-        memberClient.write(`true`);
+        const [checkType, checkID, checkPW] = String(data).split('#');
+        console.log(`${checkType}, ${checkID}, ${checkPW}`);
+        if (checkType === 'SignIn') {
+            // 파일을 불러오기
+            // 탐색을 통해서 ID, PW 조회
+            if (checkID === 'hyodol' && checkPW === '1234') memberClient.write('true');
+            else memberClient.write('false'); 
+        } else if (checkType === 'SignUp') {
+            // 파일을 불러와서 탐색을 통해서 ID 조회
+            // 이미 존재하면 false
+            // 존재하지 않으면 저장..
+            memberClient.write('true');
+        }
     });
 
     memberClient.on('close', () => {
@@ -23,6 +37,4 @@ memberServer.on('connection', (memberClient) => {
 
 memberServer.on('error', (error) => { console.error(JSON.stringify(error)); });
 
-memberServer.on('close', () => { console.log(`Member Server socket is closed.`); });
-
-memberServer.listen(60000, () => { console.log(`TCP Server listen on address ${JSON.stringify(memberServer.address())}`); });
+memberServer.on('close', () => { console.log(`[Member] Server socket is closed.`); });
