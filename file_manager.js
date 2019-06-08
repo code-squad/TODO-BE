07b2c98225bd40b127ec15fd223acc4bed3828c7
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-08 22:47:13 +0900

    test: Simulate a sign in function
    - Use a pbkdf2 function of crpyto module (for encryption)

--- file_manager.js
+++ file_manager.js
@@ -2,39 +2,64 @@ const createCSVWriter = require('csv-writer').createObjectCsvWriter;
 const crypto = require('crypto');
 const fs = require('fs');
 
+let isMemberOK = false;
+
 const csvWriter = createCSVWriter({ 
     path: './member_information.csv',
-    header: [ {id:'id', title:'ID'}, {id:'pw', title: 'PW'}, {id:'key', title: 'KEY'} ],
+    header: [ {id:'id', title:'ID'}, {id:'hashPW', title: 'HASHPW'}, {id:'salt', title: 'SALT'} ],
     append: true,
 });
 
 const readCSVFile = () => { return fs.readFileSync('./member_information.csv', 'utf-8'); }
 
-const checkID = (member, id) => { return member.split(',')[0] === id; }
-
-const isExistMember = (id) => {
+const isExist = (id) => {
     const members = readCSVFile();
     for (const member of members.split('\n')) {
-        if (checkID(member, id)) return true;
+        if (member.split(',')[0] === id) return true;
     }
     return false;
 }
 
-const writeCSVFile = (id, pw) => {
-    const byteLength = 64, loopCount = 108236;
-    crypto.randomBytes(byteLength, (err, buf) => {
-        const key = buf.toString('base64');
-        crypto.pbkdf2(pw, key, loopCount, byteLength, 'sha512', (err, encryptedPW) => {
-            csvWriter.writeRecords([{'id': id, 'pw': encryptedPW.toString('base64'), 'key': key}])
-                        .then(() => { console.log(`The CSV file was written successfully`); });     
+const encrypt = (pw) => {
+    return new Promise((resolve) => {
+        crypto.randomBytes(64, (err, buf) => {
+            const salt = buf.toString('base64');
+            crypto.pbkdf2(pw, salt, 108236, 64, 'sha512', (err, hashPW) => { 
+                resolve([hashPW.toString('base64'), salt]);
+            });
         });
     });
 }
 
-const save = (id, pw) => {
-    if (isExistMember(id)) return false;
+const writeCSVFile = async (id, pw) => {
+    const [hashPW, salt] = await encrypt(pw);
+    csvWriter.writeRecords([{'id': id, 'hashPW': hashPW, 'salt': salt}])
+        .then(() => { console.log(`The CSV file was written successfully`); });     
+}
+
+/////////////// exports ///////////////
+module.exports.signUp = (id, pw) => {
+    if (isExist(id)) return false;
     writeCSVFile(id, pw); 
     return true;
 }
 
-module.exports = { save };
+module.exports.isMember = () => { return isMemberOK; }
+
+module.exports.signIn = (id, pw) => {
+    const members = readCSVFile();
+    for (const member of members.split('\n')) {
+        const [existID, existHashPW, salt] = member.split(',');
+        if (existID === id) {
+            crypto.pbkdf2(pw, salt, 108236, 64, 'sha512', (err, hashPW) => { 
+                if (existHashPW === hashPW.toString('base64')) {
+                    console.log(`Passwords match.`);
+                    isMemberOK = true;
+                } else {
+                    console.log(`Passwords do not match`);
+                    isMemberOK = false;
+                }
+            });
+        }
+    }
+}
