e821a9045dd685d3f5a48c8adfc2f1815d665d09
Sony <sonypark0204@gmail.com>
2019-06-09 22:32:46 +0900

    [refactor] Create login module
    - app.js에 있는 로그인 로직을 모듈로 분리

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const rl = require('readline');
 const chalk = require('chalk');
-const Utils = require('./client/utils');
 const net = require('net');
+const Utils = require('./client/utils');
 const TodoApp = require('./client/todo_app');
+const login = require('./login');
+const register = require('./register');
 
 const inputReadline = rl.createInterface({
     input : process.stdin,
@@ -26,9 +28,9 @@ class App {
         return new Promise(resolve => {
             this.inputReadline.question('회원인가요? (yes/no)', async answer => {
                 if (answer === 'no') {
-                    resolve(this.register());
+                    resolve(register(this.inputReadline, this.socket, utils));
                 } else if (answer === 'yes') {
-                    resolve(this.login());
+                    resolve(login(this.inputReadline, this.socket, utils));
                 } else {
                     utils.errorLog('입력값이 올바르지 않습니다. yes / no 중 하나를 입력해주세요');
                     resolve(this.init());
@@ -37,60 +39,12 @@ class App {
         });
     }
 
-    register() {
-        return new Promise(resolve => {
-            this.inputReadline.question('아이디를 입력하세요', async (id) => {
-                this.socket.write(`register$id$${id}`); // 서버로 아이디 전송
-                const duplicatedID = await utils.getUserData();
-                this.socket.removeAllListeners();
-                if (duplicatedID) {
-                    utils.errorLog('이미 사용중인 아이디 입니다.');
-                    resolve(this.register());
-                } else {
-                    return this.inputReadline.question('비밀번호를 입력하세요', async (pw) => {
-                        this.socket.write(`register$pw$${id}&${pw}`); // 서버로 아이디, 비밀번호 전송
-                        const register_success = await utils.getUserData();
-                        this.socket.removeAllListeners();
-                        if (register_success) {
-                            console.log('------------- 회원가입이 완료됐습니다. -------------');
-                            console.log('------------- 로그인 창으로 이동합니다. ------------');
-                            resolve(this.login());
-                        } else {
-                            utils.errorLog('입력한 정보가 올바르지 않습니다.');
-                            resolve(this.register());
-                        }
-                    });
-                }
-            })
-        })
-    }
-
-    login() {
-        return new Promise(resolve => {
-            this.inputReadline.question('아이디를 입력하세요', (id) => {
-                console.log(id);
-                return this.inputReadline.question('비밀번호를 입력하세요', async (pw) => {
-                    console.log(pw);
-                    this.socket.write(`login$id_pw$${id}&${pw}`); // 서버로 아이디, 비밀번호 전송
-                    const {login_success, login_user_id} = await utils.getUserData();
-                    this.socket.removeAllListeners();
-                    if (login_success) {
-                        resolve(login_user_id);
-                    } else {
-                        utils.errorLog('입력한 정보가 올바르지 않습니다.');
-                        resolve(this.login());
-                    }
-                });
-            });
-        })
-    }
-
     mainExecutor(login_user_id) {
         this.inputReadline.setPrompt('명령어를 입력하세요(도움말은 help / 종료하려면 q를 누르세요): ');
         this.inputReadline.prompt();
         this.inputReadline.on('line', function (line) {
 
-            if (line === "q") this.inputReadline.close();
+            if (line === "q") inputReadline.close(); // this 를 붙이면 왜 오류가 날까..?
             app.checkCommands(line, login_user_id);
 
         })
@@ -143,7 +97,7 @@ const app = new App(socket, inputReadline);
 async function asyncTest() {
     const login_user_id = await app.init();
     console.log(`------------- 로그인 되었습니다. -------------`);
-    console.log(`<<< ${login_user_id}님, TODO APP에 오신 걸 환경합니다! >>>`);
+    console.log(`<<< ${login_user_id}님, TODO APP에 오신 걸 환영합니다! >>>`);
     app.mainExecutor(login_user_id);
 }
 
