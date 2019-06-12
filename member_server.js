63ede28b26c1016a37ff61202f90ed6588d1a2a6
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-12 16:15:51 +0900

    fix: 'Sign in' bug problem

--- member_server.js
+++ member_server.js
@@ -4,25 +4,17 @@ const net = require('net');
 const memberServer = net.createServer();
 const memberClientSocket = [];
 
-memberServer.setMaxListeners(15);
-
-memberServer.listen(60000, () => { console.log(`Server listen on address -> ${JSON.stringify(memberServer.address())}`); });
+memberServer.listen(60000, () => { 
+    console.log(`[Member] Server listen on address -> ${JSON.stringify(memberServer.address())}`); 
+});
 
 memberServer.on('connection', (memberClient) => {
-    console.log(`local address -> ${memberClient.localAddress}, local port -> ${memberClient.localPort}`);
-    console.log(`remote address -> ${memberClient.remoteAddress}, remote port -> ${memberClient.remotePort}`);
+    console.log(`[Member] local address -> ${memberClient.localAddress}, local port -> ${memberClient.localPort}`);
+    console.log(`[Member] remote address -> ${memberClient.remoteAddress}, remote port -> ${memberClient.remotePort}`);
 
     memberClientSocket.push(memberClient);
 
-    memberClient.on('data', (data) => {
-        const [type, id, pw] = String(data).split('#');
-        if (type === 'SignIn') {
-            fileManager.signIn(id, pw);
-        } else if (type === 'SignUp') {
-            const isSignUp = String(fileManager.signUp(id, pw));
-            memberClient.write(isSignUp);
-        }
-    });
+    memberClient.on('data', (data) => { sign(memberClient, data); });
 
     memberClient.on('close', () => {
         const index = memberClientSocket.indexOf(memberClient);
@@ -34,3 +26,8 @@ memberServer.on('connection', (memberClient) => {
 memberServer.on('error', (error) => { console.error(JSON.stringify(error)); });
 
 memberServer.on('close', () => { console.log(`[Member] Server socket is closed.`); });
+
+const sign = async (memberClient, data) => {
+    const [type, id, pw] = String(data).split('#');
+    memberClient.write(String(await fileManager.sign(type, id, pw)));
+} 
