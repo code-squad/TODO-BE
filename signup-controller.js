4a4660c0ec1d8cb1f06d6959258179386bce5fd2
Jimin Yeon <gmini.y@gmail.com>
2019-06-17 17:27:58 +0900

    [feat] signup 이미 존재하는 id erro 구현

--- signup-controller.js
+++ signup-controller.js
@@ -1,6 +1,11 @@
 const model = require('./model');
 const signupController = {
     saveUser(id, password) {
+        const memberList = require('./member-list');
+        if(memberList[id]) {
+            alert("이미 존재하는 아이디입니다. 다른 아이디를 사용해주세요.");
+            return;
+        }
         model.updateMemberList(id, password);
     }
 }
