9707f2e9edcd6762e6e85222a5a3b65d605e9f5a
Jimin Yeon <gmini.y@gmail.com>
2019-06-17 18:16:14 +0900

    [feat] signup 성공시 login page로 이동

--- signup-controller.js
+++ signup-controller.js
@@ -4,9 +4,10 @@ const signupController = {
         const memberList = require('./member-list');
         if(id in memberList) {
             alert("이미 존재하는 아이디입니다. 다른 아이디를 사용해주세요.");
-            return;
+            return false;
         }
         model.updateMemberList(id, password);
+        return true;
     }
 }
 
--- signup.js
+++ signup.js
@@ -4,5 +4,8 @@ const signupButton = document.querySelector('#signup-button');
 signupButton.addEventListener('click', () => {
     const id = document.querySelector('#id-box').value;
     const password = document.querySelector('#password-box').value;
-    signupController.saveUser(id, password);
+    if(signupController.saveUser(id, password)) {
+        alert("회원가입이 완료되었습니다. 로그인해주세요");
+        window.location.href = './login.html';
+    }
 });
