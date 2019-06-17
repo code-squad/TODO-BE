dd5eaf77ad102792f5372d0339e7265a3a440f27
Jimin Yeon <gmini.y@gmail.com>
2019-06-17 16:27:02 +0900

    [feat] signup method init
    - signup-controller 생성
    - signup button click 시 signup method 실행

---
+++ signup-controller.js
@@ -0,0 +1,7 @@
+const signupController = {
+    signup(id, password) {
+        console.log(id, password);
+    }
+}
+
+module.exports = signupController;
--- signup.js
+++ signup.js
@@ -1,8 +1,10 @@
+const signupController = require('./signup-controller');
 const signupButton = document.querySelector('#signup-button');
 
 signupButton.addEventListener('click', () => {
     const id = document.querySelector('#id-box').value;
     const password = document.querySelector('#password-box').value;
-    console.log(id, password);
+    signupController.signup(id, password);
+
 });
 
