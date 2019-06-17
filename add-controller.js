47f1f1ea367a5080c32910fd86e1a93c2c45799f
Jimin Yeon <gmini.y@gmail.com>
2019-06-17 21:25:59 +0900

    [feat] item add 시 data 저장
    - add controller 에 add method 구현
    - user의 item list를 받아와서 new item push 한 후에 model 에 update 요청

--- add-controller.js
+++ add-controller.js
@@ -1,6 +1,10 @@
+const model = require('./model');
 const addController = {
     add(user, [name, status, id]) {
-        console.log(user, name, status, id);
+        const items = require('./item-list')[user];
+        const item = {'name': name, 'status': status, 'id': id};
+        items.push(item);
+        model.updateItemList(user, items);
     }
 }
 
