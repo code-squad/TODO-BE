68f0163e2455986239b91978cd8702603dedf037
Jimin Yeon <gmini.y@gmail.com>
2019-06-18 18:19:45 +0900

    [feat] 회원가입시 id/pw 공란이면 error 출력

--- signup.js
+++ signup.js
@@ -1,9 +1,18 @@
 const signupController = require('./signup-controller');
 const signupButton = document.querySelector('#signup-button');
+const idBox = document.querySelector('#id-box');
+const passwordBox = document.querySelector('#password-box');
+
+idBox.focus();
+
 
 signupButton.addEventListener('click', () => {
-    const id = document.querySelector('#id-box').value;
-    const password = document.querySelector('#password-box').value;
+    const id = idBox.value;
+    const password = passwordBox.value;
+    if (!(password && id)) {
+        alert("아이디와 패스워드를 입력해주세요.");
+        return;
+    }
     if(signupController.saveUser(id, password)) {
         alert("회원가입이 완료되었습니다. 로그인해주세요");
         window.location.href = './login.html';
